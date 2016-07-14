26ba4a906cea6f7a6c87489e529471cb99deb730
Atul Varma <varmaa@gmail.com>
2016-07-14 08:23:33 -0400

    Add drag-over styling for upload widget.

--- hourglass_site/static_source/js/common/upload.js
+++ hourglass_site/static_source/js/common/upload.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
   $('.upload').each(function () {
     var $el = $(this);
     var $input = $('input', $el);
+    var dragCounter = 0;
     var self = {
       input: $input[0],
       isDegraded: false,
@@ -62,10 +63,22 @@ $(document).ready(function () {
       return;
     }
 
-    $el.on('dragenter', stopAndPrevent);
+    $el.on('dragenter', function (e) {
+      stopAndPrevent(e);
+
+      dragCounter++;
+      $el.addClass('dragged-over');
+    });
+    $el.on('dragleave', function (e) {
+      // http://stackoverflow.com/a/21002544/2422398
+      if (--dragCounter === 0) {
+        $el.removeClass('dragged-over');
+      }
+    });
     $el.on('dragover', stopAndPrevent);
     $el.on('drop', function (e) {
       stopAndPrevent(e);
+      $el.removeClass('dragged-over');
 
       var dt = e.originalEvent.dataTransfer;
       var files = dt.files;
