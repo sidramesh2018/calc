f0c6d828bc558a049888fc12c6237393c15e7c0a
James Seppi <james.seppi@gsa.gov>
2016-07-15 15:25:56 -0500

    improve output, add verbose mode

--- meta/management/commands/ultratest.py
+++ meta/management/commands/ultratest.py
@@ -1,9 +1,30 @@
-import djclick as click
+import os
 import subprocess
+import djclick as click
 
 
 @click.command()
-def command():
+@click.option('--verbose', is_flag=True)
+def command(verbose):
     click.echo('Running ALL THE TESTS')
-    subprocess.call('py.test')
-    subprocess.call('npm run eslint', shell=True)
+
+    tests = [
+        {'name': 'py.test', 'cmd': 'py.test'},
+        {'name': 'flake8',  'cmd': 'flake8 --exclude=node_modules .'},
+        {'name': 'eslint',  'cmd': 'npm run failable-eslint'},
+    ]
+
+    out = None
+    if not verbose:
+        out = open(os.devnull, 'w')
+
+    for entry in tests:
+        click.echo('-> %s ' % entry['name'], nl=(out is None))
+        entry['result'] = subprocess.call(
+            entry['cmd'], stdout=out, stderr=subprocess.STDOUT, shell=True
+        )
+        if not verbose:
+            if entry['result'] is 0:
+                click.secho('OK', fg='green')
+            else:
+                click.secho('FAIL', fg='red')
