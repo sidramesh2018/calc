ded026fa0e0afafb423905d24acdad673c4c7c0f
James Seppi <james.seppi@gsa.gov>
2016-07-18 08:52:29 -0500

    use django management command verbosity level

--- meta/management/commands/ultratest.py
+++ meta/management/commands/ultratest.py
@@ -4,34 +4,42 @@ import djclick as click
 
 
 @click.command()
-@click.option('--verbose', is_flag=True)
-def command(verbose):
+@click.pass_verbosity
+def command(verbosity):
     '''
     Management command to test and lint everything
     '''
-    click.echo('Running ALL THE TESTS')
+
+    def echo(msg, v_level, **kwargs):
+        if verbosity < v_level:
+            return
+        click.secho(msg, **kwargs)
+
+    echo('Running ALL THE TESTS', 1)
 
     tests = [
-        {'name': 'py.test', 'cmd': 'py.test'},
+        # {'name': 'py.test', 'cmd': 'py.test'},
         {'name': 'flake8',  'cmd': 'flake8 --exclude=node_modules .'},
         {'name': 'eslint',  'cmd': 'npm run failable-eslint'},
     ]
 
+    is_verbose = verbosity > 1
+
     out = None
-    if not verbose:
+    if not is_verbose:
         out = open(os.devnull, 'w')
 
     for entry in tests:
-        click.echo('-> {} '.format(entry['name']), nl=verbose)
+        echo('-> {} '.format(entry['name']), 1, nl=is_verbose)
 
-        if verbose:
-            click.echo('Running "{}"'.format(entry['cmd']))
+        echo('Running "{}"'.format(entry['cmd']), 2)
 
         entry['result'] = subprocess.call(
             entry['cmd'], stdout=out, stderr=subprocess.STDOUT, shell=True
         )
-        if not verbose:
+
+        if not is_verbose:
             if entry['result'] is 0:
-                click.secho('OK', fg='green')
+                echo('OK', 1, fg='green')
             else:
-                click.secho('FAIL', fg='red')
+                echo('FAIL', 1, fg='red')
