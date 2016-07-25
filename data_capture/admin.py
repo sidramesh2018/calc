cede1f9f6e5cc63a6da40b2628be7476dbde77de
Atul Varma <varmaa@gmail.com>
2016-07-25 17:06:39 -0400

    prohibit add/delete on submitted price list rows in admin.

--- data_capture/admin.py
+++ data_capture/admin.py
@@ -15,6 +15,12 @@ class SubmittedPriceListRowInline(admin.TabularInline):
         models.TextField: {'widget': forms.TextInput}
     }
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(SubmittedPriceList)
 class SubmittedPriceListAdmin(admin.ModelAdmin):
