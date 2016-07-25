42e76b8564ac3e4fd7007816c4a9f9ea6633785f
James Seppi <james.seppi@gmail.com>
2016-07-25 11:48:29 -0500

    July 2016 Hourly Prices Update (#444)
    * add hourly prices from July 2016 export
    * rm django admin, upgrade to latest 1.7 release
    * add overflow-wrap to filters spans

--- hourglass/urls.py
+++ hourglass/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
@@ -12,8 +11,6 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^api/', include(api_urls)),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^tests/$', TemplateView.as_view(template_name='tests.html')),
     url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /")),
 )
-
