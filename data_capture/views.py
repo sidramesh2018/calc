a799a55b960fd24c85fedbf471f04cc0cf6ce8d3
James Seppi <james.seppi@gsa.gov>
2016-07-29 11:44:24 -0500

    return special ajax version of step_1_form

--- data_capture/views.py
+++ data_capture/views.py
@@ -46,7 +46,7 @@ def step_1(request):
     if request.is_ajax():
         return JsonResponse({
             'form_html': render_to_string(
-                'data_capture/step_1_form.html',
+                'data_capture/step_1_form_ajax.html',
                 RequestContext(request, ctx)
             )
         })
