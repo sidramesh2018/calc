9e701ce82788c4ab8850ff2ad304cb05411614c5
James Seppi <james.seppi@gsa.gov>
2016-08-01 09:25:32 -0500

    fix attachment of sortBy functions and default sort, fixes #488

--- hourglass_site/static_source/js/data-capture/tablesort.js
+++ hourglass_site/static_source/js/data-capture/tablesort.js
@@ -3,31 +3,18 @@
 const $ = jQuery;
 const sortableTables = document.querySelectorAll('.table-sortable');
 
-function sortInts(intCol) {
-  $(intCol).data('sortBy', (th, td) => parseInt(td.text(), 10));
-}
-
-function sortFloats(floatCol) {
-  $(floatCol).data('sortBy', (th, td) => parseFloat(td.text()));
-}
-
 $(document).ready(() => {
   for (const table of sortableTables) {
-    $(table).tablesort();
+    const $table = $(table);
+    $table.tablesort();
 
     // Floats should be sorted as numbers, not strings
-    [].forEach.call(table.getElementsByClassName('js-float'), function sort() {
-      sortFloats(this);
-    });
+    $('.js-float', $table).data('sortBy', (th, td) => parseFloat(td.text()));
 
     // Don't sort ints as strings, either
-    [].forEach.call(table.getElementsByClassName('js-int'), function sort() {
-      sortInts(this);
-    });
+    $('.js-int', $table).data('sortBy', (th, td) => parseInt(td.text(), 10));
 
     // Sort by default column, if present
-    [].forEach.call(table.getElementsByClassName('js-default-sort'), () => {
-      $(table).data('tablesort').sort($('.js-default-sort'));
-    });
+    $table.data('tablesort').sort($('.js-default-sort', $table));
   }
 });
