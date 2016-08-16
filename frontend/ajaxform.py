964df70914aa650feb734762810781d89c81fc51
Atul Varma <varmaa@gmail.com>
2016-08-16 19:07:06 -0400

    Call render_to_string() in a non-deprecated way.

--- frontend/ajaxform.py
+++ frontend/ajaxform.py
@@ -42,11 +42,11 @@ def render(request, context, template_name, ajax_template_name):
     into it.
     '''
 
-    final_context = RequestContext(request, context)
-
     if request.is_ajax():
         return JsonResponse({
-            'form_html': render_to_string(ajax_template_name, final_context)
+            'form_html': render_to_string(ajax_template_name, context,
+                                          request=request)
         })
 
-    return HttpResponse(render_to_string(template_name, final_context))
+    return HttpResponse(render_to_string(template_name, context,
+                                         request=request))
