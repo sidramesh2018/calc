1e42e82d59ed32edf46b5d29bcd96f6d4f03174a
Atul Varma <varmaa@gmail.com>
2016-08-16 13:53:54 -0400

    Add date field/widget to style guide.

--- styleguide/urls.py
+++ styleguide/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from . import views, ajaxform_example
+from . import views, ajaxform_example, date_example
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
     url(r'^ajaxform$', ajaxform_example.view, name='ajaxform'),
+    url(r'^date$', date_example.view, name='date'),
 )
