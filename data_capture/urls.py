d8f0c3215d39718bee9c24cf279efbad53c476dd
Heather Battaglia <heather.battaglia@gsa.gov>
2016-08-24 10:01:13 -0600

    add url for step 5

--- data_capture/urls.py
+++ data_capture/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^step/2$', views.step_2, name='step_2'),
     url(r'^step/3$', views.step_3, name='step_3'),
     url(r'^step/4$', views.step_4, name='step_4'),
+    url(r'^step/5$', views.step_5, name='step_5'),
 ]
