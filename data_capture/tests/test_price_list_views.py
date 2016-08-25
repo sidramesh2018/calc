2c645aa6745e2a4423b810b5f64fd6c925f8dd1d
Heather Battaglia <heather.battaglia@gsa.gov>
2016-08-25 11:20:38 -0600

    wip price list tests; step 1 tests

--- data_capture/tests/test_price_list_views.py
+++ data_capture/tests/test_price_list_views.py
@@ -21,13 +21,11 @@ class PriceListStepTestCase(StepTestCase):
 class Step1Tests(PriceListStepTestCase):
     url = '/data-capture/step/1'
 
-    csvpath = FAKE_SCHEDULE_EXAMPLE_PATH
-
-    def ajax_post(self, data):
-        res = self.client.post(self.url, data,
-                               HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        self.assertEqual(res.status_code, 200)
-        return res, json.loads(res.content.decode('utf-8'))
+    valid_form = {
+        'schedule': FAKE_SCHEDULE,
+        'contract_number': 'GS-123-4567',
+        'vendor_name': 'foo'
+    }
 
     def test_login_is_required(self):
         self.assertRedirectsToLogin(self.url)
@@ -36,6 +34,45 @@ class Step1Tests(PriceListStepTestCase):
         self.login()
         res = self.client.get(self.url)
         self.assertEqual(res.status_code, 200)
+
+    def test_valid_post_redirects_to_step_2(self):
+        self.login()
+        res = self.client.post(self.url, self.valid_form)
+        self.assertRedirects(res, Step2Tests.url)
+
+    def test_invalid_post_returns_html(self):
+        self.login()
+        res = self.client.post(self.url, {})
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, 'This field is required')
+        self.assertHasMessage(
+            res,
+            'error',
+            'Oops, please correct the errors below and try again.'
+        )
+
+
+class Step2Tests(PriceListStepTestCase):
+    url = '/data-capture/step/2'
+
+    def test_login_is_required(self):
+        self.assertRedirectsToLogin(self.url)
+
+    def test_gleaned_data_is_required(self):
+        self.login()
+        res = self.client.get(self.url)
+        self.assertRedirects(res, Step1Tests.url)
+
+
+class Step3Tests(PriceListStepTestCase):
+    url = '/data-capture/step/3'
+    csvpath = FAKE_SCHEDULE_EXAMPLE_PATH
+
+    def ajax_post(self, data):
+        res = self.client.post(self.url, data,
+                               HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(res.status_code, 200)
+        return res, json.loads(res.content.decode('utf-8'))
 
     def test_valid_post_sets_session_data(self):
         self.login()
@@ -57,68 +94,8 @@ class Step1Tests(PriceListStepTestCase):
                 'years_experience': '7'
             }])
 
-    def test_valid_post_redirects_to_step_2(self):
-        self.login()
-        with open(self.csvpath) as f:
-            res = self.client.post(self.url, {
-                'schedule': FAKE_SCHEDULE,
-                'file': f
-            })
-            self.assertRedirects(res, Step2Tests.url)
 
-    def test_valid_post_via_xhr_returns_json(self):
-        self.login()
-        with open(self.csvpath) as f:
-            res, json_data = self.ajax_post({
-                'schedule': FAKE_SCHEDULE,
-                'file': f
-            })
-            self.assertEqual(json_data, {
-                'redirect_url': '/data-capture/step/2'
-            })
-
-    def test_invalid_post_returns_html(self):
-        self.login()
-        res = self.client.post(self.url, {
-            'schedule': FAKE_SCHEDULE
-        })
-        self.assertEqual(res.status_code, 200)
-        self.assertContains(res, r'<!DOCTYPE html>')
-        self.assertContains(res, r'This field is required')
-        self.assertHasMessage(
-            res,
-            'error',
-            'Oops, please correct the error below and try again.'
-        )
-
-    def test_invalid_post_via_xhr_returns_json(self):
-        self.login()
-        res, json_data = self.ajax_post({'schedule': FAKE_SCHEDULE})
-        assert '<!DOCTYPE html>' not in json_data['form_html']
-        self.assertRegexpMatches(json_data['form_html'],
-                                 r'This field is required')
-        self.assertHasMessage(
-            res,
-            'error',
-            'Oops, please correct the error below and try again.'
-        )
-
-
-class Step2Tests(PriceListStepTestCase):
-    url = '/data-capture/step/2'
-
-    def test_login_is_required(self):
-        self.assertRedirectsToLogin(self.url)
-
-    def test_gleaned_data_is_required(self):
-        self.login()
-        res = self.client.get(self.url)
-        self.assertRedirects(res, Step1Tests.url)
-
-
-class Step3Tests(PriceListStepTestCase):
-    url = '/data-capture/step/3'
-
+    # old stuff
     rows = [{
         'education': 'Bachelors',
         'price': '15.00',
