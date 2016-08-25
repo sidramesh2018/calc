cdde3b5acbafee463d3571a510a287217a3425ab
Atul Varma <varmaa@gmail.com>
2016-08-25 09:04:07 -0400

    Process bulk uploads in a separate thread.

--- data_capture/views/bulk_upload.py
+++ data_capture/views/bulk_upload.py
@@ -1,6 +1,10 @@
+import logging
+import threading
+import traceback
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
-
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
 from django.db import transaction
@@ -11,6 +15,9 @@ from .common import add_generic_form_error
 from frontend import ajaxform
 from contracts.loaders.region_10 import Region10Loader
 from contracts.models import Contract, BulkUploadContractSource
+
+
+contracts_logger = logging.getLogger('contracts')
 
 
 @user_passes_test(lambda u: u.is_staff)
@@ -79,18 +86,12 @@ def region_10_step_2(request):
     })
 
 
-@user_passes_test(lambda u: u.is_staff)
 @transaction.atomic
-def region_10_step_3(request):
-    '''Load data and show success screen'''
-    upload_source_id = request.session.get('data_capture:upload_source_id')
-    if upload_source_id is None:
-        return redirect('data_capture:bulk_region_10_step_1')
-
-    upload_source = BulkUploadContractSource.objects.get(pk=upload_source_id)
-
+def process_bulk_upload(upload_source):
     file = ContentFile(upload_source.original_file)
     converter = Region10SpreadsheetConverter(file)
+
+    contracts_logger.info("Deleting contract objects related to region 10.")
 
     # Delete existing contracts identified by the same
     # procurement_center
@@ -101,6 +102,8 @@ def region_10_step_3(request):
     contracts = []
     bad_rows = []
 
+    contracts_logger.info("Generating new contract objects.")
+
     for row in converter.convert_next():
         try:
             c = Region10Loader.make_contract(row, upload_source=upload_source)
@@ -108,8 +111,12 @@ def region_10_step_3(request):
         except (ValueError, ValidationError) as e:
             bad_rows.append(row)
 
+    contracts_logger.info("Saving new contract objects.")
+
     # Save new contracts
     Contract.objects.bulk_create(contracts)
+
+    contracts_logger.info("Updating full-text search indexes.")
 
     # Update search field on Contract models
     Contract._fts_manager.update_search_field()
@@ -118,11 +125,63 @@ def region_10_step_3(request):
     upload_source.has_been_loaded = True
     upload_source.save()
 
+    return len(contracts), len(bad_rows)
+
+
+def process_bulk_upload_in_separate_thread(upload_source_id):
+    def run():
+        contracts_logger.info(
+            "Starting bulk upload processing (pk=%d)." % upload_source_id
+        )
+        upload_source = BulkUploadContractSource.objects.get(
+            pk=upload_source_id
+        )
+        ctx = {'upload_source': upload_source}
+        successful = False
+        try:
+            num_contracts, num_bad_rows = process_bulk_upload(upload_source)
+            ctx['num_contracts'] = num_contracts
+            ctx['num_bad_rows'] = num_bad_rows
+            kablooey
+            successful = True
+        except:
+            contracts_logger.exception(
+                'An exception occurred during bulk upload processing '
+                '(pk=%d).' % upload_source_id
+            )
+            ctx['traceback'] = traceback.format_exc()
+        ctx['successful'] = successful
+        send_mail(
+            subject='CALC Region 10 bulk data results - upload #%d' % (
+                upload_source.id,
+            ),
+            message=render_to_string(
+                'data_capture/bulk_upload/region_10_email.txt',
+                ctx
+            ),
+            from_email=None,
+            recipient_list=[upload_source.submitter.email]
+        )
+        contracts_logger.info(
+            "Ending bulk upload processing (pk=%d)." % upload_source_id
+        )
+
+    thread = threading.Thread(target=run)
+    thread.start()
+
+
+@user_passes_test(lambda u: u.is_staff)
+def region_10_step_3(request):
+    '''Load data and show success screen'''
+    upload_source_id = request.session.get('data_capture:upload_source_id')
+    if upload_source_id is None:
+        return redirect('data_capture:bulk_region_10_step_1')
+
+    process_bulk_upload_in_separate_thread(upload_source_id)
+
     # remove the upload_source_id from session
     del request.session['data_capture:upload_source_id']
 
     return render(request, 'data_capture/bulk_upload/region_10_step_3.html', {
         'step_number': 3,
-        'num_bad_rows': len(bad_rows),
-        'num_contracts': len(contracts),
     })
