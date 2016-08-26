62e7393656f8a379052cb7a2afa0c1a028763b2b
Atul Varma <varmaa@gmail.com>
2016-08-26 17:32:27 -0400

    Add tests for approve/unapprove admin actions.

--- data_capture/tests/test_admin.py
+++ data_capture/tests/test_admin.py
@@ -1,16 +1,16 @@
+from unittest.mock import patch
+from django.contrib import messages
 from django.test import override_settings
 
+from .. import admin, models
 from .common import FAKE_SCHEDULE
 from .test_models import ModelTestCase
 
 
 @override_settings(
     DATA_CAPTURE_SCHEDULES=[FAKE_SCHEDULE],
-    # Strangely, disabling DEBUG mode silences some errors in Django
-    # admin views, so we'll enforce it so that any errors are raised.
-    DEBUG=True
 )
-class AdminTests(ModelTestCase):
+class AdminTestCase(ModelTestCase):
     def setUp(self):
         super().setUp()
         self.price_list = self.create_price_list()
@@ -21,6 +21,13 @@ class AdminTests(ModelTestCase):
     def setup_user(self):
         self.user = self.login(is_superuser=True)
 
+
+@override_settings(
+    # Strangely, disabling DEBUG mode silences some errors in Django
+    # admin views, so we'll enforce it so that any errors are raised.
+    DEBUG=True,
+)
+class ViewTests(AdminTestCase):
     def test_submittedpricelistrow_list_returns_200(self):
         res = self.client.get('/admin/data_capture/submittedpricelistrow/')
         self.assertEqual(res.status_code, 200)
@@ -45,3 +52,48 @@ class AdminTests(ModelTestCase):
             )
         )
         self.assertEqual(res.status_code, 200)
+
+
+@patch.object(messages, 'add_message')
+class ActionTests(AdminTestCase):
+    def test_approve_ignores_approved_price_lists(self, mock):
+        self.price_list.approve()
+        admin.approve(None, "fake request",
+                      models.SubmittedPriceList.objects.all())
+        mock.assert_called_once_with(
+            "fake request",
+            messages.INFO,
+            '0 price list(s) have been approved and added to CALC.'
+        )
+
+    def test_unapprove_ignores_unapproved_price_lists(self, mock):
+        admin.unapprove(None, "fake request",
+                        models.SubmittedPriceList.objects.all())
+        mock.assert_called_once_with(
+            "fake request",
+            messages.INFO,
+            '0 price list(s) have been unapproved and removed from CALC.'
+        )
+
+    def test_approve_works(self, mock):
+        admin.approve(None, "fake request",
+                      models.SubmittedPriceList.objects.all())
+        mock.assert_called_once_with(
+            "fake request",
+            messages.INFO,
+            '1 price list(s) have been approved and added to CALC.'
+        )
+        self.price_list.refresh_from_db()
+        self.assertTrue(self.price_list.is_approved)
+
+    def test_unapprove_works(self, mock):
+        self.price_list.approve()
+        admin.unapprove(None, "fake request",
+                        models.SubmittedPriceList.objects.all())
+        mock.assert_called_once_with(
+            "fake request",
+            messages.INFO,
+            '1 price list(s) have been unapproved and removed from CALC.'
+        )
+        self.price_list.refresh_from_db()
+        self.assertFalse(self.price_list.is_approved)
