4437dd49b4e613b7d4012a6d317b21bfe5605246
Atul Varma <varmaa@gmail.com>
2016-09-02 10:10:45 -0400

    Add <collapsible-area> web component.

---
+++ frontend/source/js/data-capture/collapsible-area.js
@@ -0,0 +1,55 @@
+/* global window, document */
+
+import 'document-register-element';
+
+import * as supports from './feature-detection';
+
+const KEY_SPACE = 32;
+const KEY_ENTER = 13;
+
+/**
+ * CollapsibleArea represents a <collapsible-area> web component.
+ */
+
+class CollapsibleArea extends window.HTMLElement {
+  attachedCallback() {
+    if ('isUpgraded' in this) {
+      // We've already been attached.
+      return;
+    }
+
+    this.expander = this.firstElementChild;
+
+    this.isUpgraded = this.expander &&
+                      'classList' in this &&
+                      !supports.isForciblyDegraded(this);
+
+    if (this.isUpgraded) {
+      this.expander.setAttribute('aria-expanded', 'false');
+      this.expander.setAttribute('role', 'button');
+      this.expander.setAttribute('tabindex', '0');
+      this.expander.addEventListener('click', this.toggle.bind(this), false);
+      this.expander.addEventListener('keyup', e => {
+        if (e.keyCode === KEY_SPACE || e.keyCode === KEY_ENTER) {
+          this.toggle();
+        }
+      }, true);
+    }
+  }
+
+  toggle() {
+    if (!this.isUpgraded) {
+      return;
+    }
+
+    const newVal = this.expander.getAttribute('aria-expanded') === 'true'
+                   ? 'false' : 'true';
+    this.expander.setAttribute('aria-expanded', newVal);
+  }
+}
+
+CollapsibleArea.prototype.SOURCE_FILENAME = __filename;
+
+document.registerElement('collapsible-area', {
+  prototype: CollapsibleArea.prototype,
+});
