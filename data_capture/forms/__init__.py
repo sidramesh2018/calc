019846a846928b77a0b69361ca9943c2acb5ac7c
Heather Battaglia <heather.battaglia@gsa.gov>
2016-09-07 15:04:28 -0500

    aaaaargh line length

--- data_capture/forms/__init__.py
+++ data_capture/forms/__init__.py
@@ -2,4 +2,5 @@
 from .price_list import Step1Form, Step2Form, Step3Form, Step4Form
 from .bulk_upload import Region10BulkUploadForm
 
-__all__ = ['Step1Form', 'Step2Form', 'Step3Form', 'Step4Form', 'Region10BulkUploadForm']
+__all__ = ['Step1Form', 'Step2Form', 'Step3Form', 'Step4Form',
+           'Region10BulkUploadForm']
