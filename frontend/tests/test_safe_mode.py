194736458793ac276726b25984c3e7af92db3926
Atul Varma <varmaa@gmail.com>
2016-09-16 06:39:30 -0400

    Add more tests to test_safe_mode.py.

--- frontend/tests/test_safe_mode.py
+++ frontend/tests/test_safe_mode.py
@@ -1,7 +1,17 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import patch
+import django.test
+
+from .. import safe_mode
+from ..context_processors import is_safe_mode_enabled
 
 
-class SafeModeTests(TestCase):
+class FakeRequest:
+    def __init__(self, **kwargs):
+        self.session = kwargs
+
+
+class UrlTests(django.test.TestCase):
     def test_enable_works(self):
         res = self.client.post('/safe-mode/enable')
         self.assertTrue(self.client.session['enable_safe_mode'])
@@ -16,3 +26,27 @@ class SafeModeTests(TestCase):
         for path in ['/safe-mode/enable', '/safe-mode/disable']:
             res = self.client.post(path, HTTP_REFERER='/foo')
             self.assertRedirects(res, '/foo', fetch_redirect_response=False)
+
+
+class SimpleTests(unittest.TestCase):
+    def test_is_enabled_returns_false_by_default(self):
+        self.assertFalse(safe_mode.is_enabled(FakeRequest()))
+
+    def test_is_enabled_returns_session_key_value(self):
+        self.assertTrue(
+            safe_mode.is_enabled(FakeRequest(enable_safe_mode=True))
+        )
+        self.assertFalse(
+            safe_mode.is_enabled(FakeRequest(enable_safe_mode=False))
+        )
+
+    @patch.object(safe_mode, 'is_enabled')
+    def test_context_processor_delegates_to_is_enabled(self, is_enabled):
+        is_enabled.return_value = 'sure buddy'
+
+        req = FakeRequest()
+        ctx = is_safe_mode_enabled(req)
+        self.assertEqual(ctx, {
+            'is_safe_mode_enabled': 'sure buddy'
+        })
+        is_enabled.assert_called_once_with(req)
