d16d9907392aeeffd0d5e02db7426e1fe11305a8
Atul Varma <varmaa@gmail.com>
2016-09-21 14:41:11 -0500

    add <uswds-date> custom element.

---
+++ frontend/source/js/data-capture/date.js
@@ -0,0 +1,46 @@
+/* global window, document */
+
+import 'document-register-element';
+
+const KEY_DASH = 189;
+const KEY_PERIOD = 190;
+const KEY_SLASH = 191;
+
+class UswdsDate extends window.HTMLElement {
+  _getNextInput(currentInput) {
+    const inputs = this.querySelectorAll('input');
+    let useNext = false;
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (useNext) {
+        return inputs[i];
+      } else if (inputs[i] === currentInput) {
+        useNext = true;
+      }
+    }
+
+    return null;
+  }
+  _onKeyDown(e) {
+    if (e.keyCode === KEY_DASH || e.keyCode === KEY_PERIOD ||
+        e.keyCode === KEY_SLASH) {
+      const nextInput = this._getNextInput(e.target);
+
+      if (nextInput) {
+        e.preventDefault();
+        nextInput.focus();
+      }
+    }
+  }
+  createdCallback() {
+    this.addEventListener('keydown', this._onKeyDown.bind(this), true);
+  }
+}
+
+UswdsDate.prototype.SOURCE_FILENAME = __filename;
+
+document.registerElement('uswds-date', {
+  prototype: UswdsDate.prototype,
+});
+
+module.exports = UswdsDate;
