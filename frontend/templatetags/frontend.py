62fbe0751c85a0349523a149097fb3f5bdc78350
Atul Varma <varmaa@gmail.com>
2016-09-28 15:07:20 -0400

    use <legend> where appropriate.

--- frontend/templatetags/frontend.py
+++ frontend/templatetags/frontend.py
@@ -1,7 +1,17 @@
-from django import template
+from django import template, forms
+from .. import date
 
 
 register = template.Library()
+
+
+# These are widget classes that consist of multiple sub-widgets. We'd
+# like to use a <legend> element with these, instead of a <label>, so
+# that screen-readers contextualize them properly.
+LEGEND_WIDGETS = (
+    date.SplitDateWidget,
+    forms.RadioSelect
+)
 
 
 @register.simple_tag(takes_context=True)
@@ -15,4 +25,5 @@ def fieldset(context, field):
 
     return t.render(template.Context({
         'field': field,
+        'use_legend': isinstance(field.field.widget, LEGEND_WIDGETS)
     }))
