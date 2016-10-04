90e07d2c05397287f21f5e21b2894e8ca80f4991
James Seppi <james.seppi@gmail.com>
2016-10-04 09:34:35 -0600

    management command to bulk create users (#846)

---
+++ meta/tests/test_bulkcreateusers.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth.models import User, Group
+
+from ..management.commands import bulkcreateusers
+
+
+class TestBulkCreateUsers(TestCase):
+    def test_get_or_create_users_works(self):
+        User.objects.create_user(
+            'test_user', email='existing_user@gsa.gov')
+        email_addresses = [
+            'user1@gsa.gov', 'user2@gsa.gov', 'existing_user@gsa.gov']
+
+        results = bulkcreateusers.get_or_create_users(email_addresses)
+        users = User.objects.all()
+
+        self.assertEqual(len(results), len(email_addresses))
+        self.assertEqual(len(results), len(users))
+        for u in users:
+            self.assertIn(u.email, email_addresses)
+
+    def test_add_users_to_group_works(self):
+        user = User.objects.create_user('test_user', email='test@gsa.gov')
+        group = Group(name='Test Group')
+        group.save()
+        bulkcreateusers.add_users_to_group(group, [user])
+        group_users = group.user_set.all()
+        self.assertEqual(len(group_users), 1)
+        self.assertIn(user, group_users)
