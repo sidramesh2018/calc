80634ab757eb048059f3ab70960de300e6cee59b
Atul Varma <varmaa@gmail.com>
2016-10-07 09:18:03 -0400

    every call to ga() proxies to window.ga().

--- frontend/source/js/common/ga.js
+++ frontend/source/js/common/ga.js
@@ -3,9 +3,16 @@
 /**
  * This is just a wrapper around Google Analytics' ga() function, which
  * should be included inline at the top of the page. If it wasn't included
- * at the top of the page, however, we'll just provide a no-op function
+ * at the top of the page, however, we'll just no-op
  * so that code which calls it still works, allowing GA to be an optional
  * dependency.
  */
 
-module.exports = typeof window.ga === 'function' ? window.ga : () => {};
+function ga(...args) {
+  if (typeof window.ga === 'function') {
+    return window.ga.apply(this, args);
+  }
+  return undefined;
+}
+
+module.exports = ga;
