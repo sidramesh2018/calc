051db6394dd5ac2d079dbcbb04b04db608c043fc
Atul Varma <varmaa@gmail.com>
2016-10-11 14:51:59 -0400

    Add smooth-scroll_tests.js.

--- frontend/source/js/data-capture/smooth-scroll.js
+++ frontend/source/js/data-capture/smooth-scroll.js
@@ -1,4 +1,4 @@
-/* global $, document, window */
+/* global $, window */
 
 /**
  * This script alters the behavior of in-page linking to smoothly
@@ -33,58 +33,82 @@
 
 // This is the amount of time, in milliseconds, that we'll take to
 // scroll the page from its current position to a new position.
-const SCROLL_MS = 500;
+const DEFAULT_SCROLL_MS = 500;
 
-function rememberCurrentScrollPosition() {
+// This determines whether we have enough functionality in the browser to
+// support smooth scrolling.
+export const IS_SUPPORTED = window.history && window.history.replaceState;
+
+function rememberCurrentScrollPosition(window) {
   window.history.replaceState({
     pageYOffset: window.pageYOffset,
   }, '');
 }
 
-function changeHash(hash, cb) {
+function changeHash(window, hash, cb) {
   const currId = window.location.hash.slice(1);
   const newId = hash.slice(1);
 
   if (currId !== newId) {
     $(window).one('hashchange', cb);
   }
-  window.location.hash = hash;
+  window.location.hash = hash;  // eslint-disable-line no-param-reassign
 }
 
-if (window.history && window.history.replaceState) {
-  $('body').on('click', 'a[href^="#"]', e => {
+function smoothScroll(window, scrollTop, scrollMs, cb) {
+  // WebKit uses `<body>` for keeping track of scrolling, while
+  // Firefox and IE use `<html>`, so we need to animate both.
+
+  const $els = $('html, body', window.document);
+  let callbacksLeft = $els.length;
+
+  $els.animate({
+    scrollTop,
+  }, scrollMs, () => {
+    // This callback is going to be called multiple times because
+    // we're animating on multiple elements, but we only want the
+    // code to execute once--hence our use of `callbacksLeft`.
+
+    if (--callbacksLeft === 0) {
+      if (cb) {
+        cb();
+      }
+    }
+  });
+}
+
+export function activate(window, options = {}) {
+  const scrollMs = options.scrollMs || DEFAULT_SCROLL_MS;
+  const onScroll = options.onScroll || (() => {});
+
+  $('body', window.document).on('click', 'a[href^="#"]', e => {
     const scrollId = $(e.target).attr('href').slice(1);
-    const scrollTarget = document.getElementById(scrollId);
-    let doneAnimating = false;
+    const scrollTarget = window.document.getElementById(scrollId);
 
     if (scrollTarget) {
       e.preventDefault();
-      rememberCurrentScrollPosition();
-      $('html, body').animate({
-        scrollTop: $(scrollTarget).offset().top,
-      }, SCROLL_MS, () => {
-        // This callback is going to be called multiple times because
-        // we're animating on multiple elements, but we only want the
-        // code to execute once--hence our use of `doneAnimating`.
-
-        if (!doneAnimating) {
-          doneAnimating = true;
-          changeHash(`#${scrollId}`, rememberCurrentScrollPosition);
-        }
+      rememberCurrentScrollPosition(window);
+      smoothScroll(window, $(scrollTarget).offset().top, scrollMs, () => {
+        changeHash(window, `#${scrollId}`, () => {
+          rememberCurrentScrollPosition(window);
+          onScroll();
+        });
       });
     }
   });
 
   window.addEventListener('popstate', e => {
     if (e.state && 'pageYOffset' in e.state) {
-      $('html, body').animate({
-        scrollTop: e.state.pageYOffset,
-      }, SCROLL_MS);
+      smoothScroll(window, e.state.pageYOffset, scrollMs, onScroll);
     }
   }, false);
 
   // https://developers.google.com/web/updates/2015/09/history-api-scroll-restoration
   if ('scrollRestoration' in window.history) {
-    window.history.scrollRestoration = 'manual';
+    window.history.scrollRestoration = 'manual';   // eslint-disable-line no-param-reassign
   }
+}
+
+if (window.history && window.history.replaceState) {
+  activate(window);
 }
