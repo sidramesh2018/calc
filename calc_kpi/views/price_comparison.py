dd47a05063ddfd3567f3e6b945b66790b06ebf41
Eric Schles <ericschles@gmail.com>
2016-11-29 15:02:20 -0500

    finished prototype for internal metric

--- calc_kpi/views/price_comparison.py
+++ calc_kpi/views/price_comparison.py
@@ -10,15 +10,21 @@ import math
 
 # Create your views here.
 def percentage_compare(request):
-    results = Contract.objects.filter(labor_category=request.labor_category)
-    total_results = len(results)
-    hourly_rate = float(request.hourly_rate)
-    count_with_higher_price = len([result for result in results if result.hourly_rate_year1 > hourly_rate])
-    percentage = math.floor(100 * (float(count_with_higher_price)/total_results))
-    print("Percentage with higer hourly year 1 rates:", percentage)
-    context = {
-        "percentage":percentage,
-        "hourly_rate":hourly_rate,
-        "labor_category":labor_category
-    }
-    return render(request, "calc_kpi/percentage_compare",context)
+    if request.method == 'POST':
+        post_data_dict = request.POST.dict()
+        results = Contract.objects.filter(labor_category=post_data_dict["labor_category"])
+        
+        total_results = len(results)
+        hourly_rate = float(post_data_dict["hourly_rate"])
+        count_with_higher_price = len([result for result in results if result.hourly_rate_year1 > hourly_rate])
+        percentage = math.floor(100 * (float(count_with_higher_price)/total_results))
+        print("Percentage with higer hourly year 1 rates:", percentage)
+        context = {
+            "percentage":percentage,
+            "hourly_rate":post_data_dict["hourly_rate"],
+            "labor_category":post_data_dict["labor_category"],
+            "result":True
+        }
+        return render(request, "calc_kpi/percentage_comparison.html",context)
+    elif request.method == "GET":
+        return render(request, "calc_kpi/percentage_comparison.html",{"result":False})
