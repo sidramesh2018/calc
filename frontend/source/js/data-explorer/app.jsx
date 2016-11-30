fe3bf699f0032b63b996f714e18135585b8c8d99
Atul Varma <varmaa@gmail.com>
2016-11-30 16:17:21 -0500

    Use mapDispatchToProps to set onClick handler.

--- frontend/source/js/data-explorer/app.jsx
+++ frontend/source/js/data-explorer/app.jsx
@@ -3,17 +3,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import RestoreExcluded from './restore-excluded';
-import { store, excludeNone } from './store';
+import { store } from './store';
 
 export default function init({ restoreExcludedRoot }) {
-  function handleClick(e) {
-    e.preventDefault();
-    store.dispatch(excludeNone());
-  }
-
   ReactDOM.render(
     <Provider store={store}>
-      <RestoreExcluded onClick={handleClick} />
+      <RestoreExcluded />
     </Provider>,
     restoreExcludedRoot
   );
--- frontend/source/js/data-explorer/restore-excluded.jsx
+++ frontend/source/js/data-explorer/restore-excluded.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { excludeNone } from './store';
+
 function RestoreExcluded({ excluded, onClick }) {
   const len = excluded.length;
   const rows = `row${len === 1 ? '' : 's'}`;
+  const handleClick = e => {
+    e.preventDefault();
+    onClick();
+  };
 
   return (
     <a className="restore"
        href="?exclude="
        style={len === 0 ? { display: 'none' } : null}
        title={`${rows}: ${excluded.join(', ')}`}
-       onClick={onClick}>
+       onClick={handleClick}>
          {len > 0 ? `★ Restore ${len} ${rows}` : ''}
     </a>
   );
@@ -27,4 +33,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(RestoreExcluded);
+const mapDispatchToProps = {
+  onClick: excludeNone,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestoreExcluded);
