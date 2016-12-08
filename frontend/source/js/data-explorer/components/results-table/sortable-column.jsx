0b2f8280825def20b18ab2a84b6dec7b296c9de9
Atul Varma <varmaa@gmail.com>
2016-12-08 12:12:46 -0500

    Refactor sortable-column.jsx a bit.

--- frontend/source/js/data-explorer/components/results-table/sortable-column.jsx
+++ frontend/source/js/data-explorer/components/results-table/sortable-column.jsx
@@ -60,72 +60,72 @@ GenericDataCell.propTypes = {
   value: React.PropTypes.any,
 };
 
+const getBaseClasses = key => ({ [`column-${key}`]: true });
+
+const createHeaderCellConnector = (description, title, key) => component => {
+  const wrappedComponent = ({ sort, setSort, children }) => {
+    // TODO: Tooltip / aria-label
+
+    const isSorted = sort.key === key;
+    const classes = Object.assign({
+      sortable: true,
+      sorted: isSorted,
+      ascending: isSorted && !sort.descending,
+      descending: isSorted && sort.descending,
+    }, getBaseClasses(key));
+
+    return React.createElement(component, {
+      toggleSort: createSortToggler(key, sort, setSort),
+      className: classNames(classes),
+      tooltip: createSortHeaderTooltip(description || title, classes),
+      title,
+    }, children);
+  };
+
+  wrappedComponent.propTypes = {
+    sort: React.PropTypes.object.isRequired,
+    setSort: React.PropTypes.func.isRequired,
+    children: React.PropTypes.any,
+  };
+
+  return connect(
+    state => ({ sort: state.sort }),
+    { setSort: setSortAction }
+  )(wrappedComponent);
+};
+
+const createDataCellConnector = key => component => {
+  const wrappedComponent = ({ sort, result, children }) => {
+    const classes = Object.assign({
+      cell: true,
+      sorted: sort.key === key,
+    }, getBaseClasses(key));
+
+    return React.createElement(component, {
+      className: classNames(classes),
+      value: result[key],
+      result,
+    }, children);
+  };
+
+  wrappedComponent.propTypes = {
+    sort: React.PropTypes.object.isRequired,
+    result: React.PropTypes.object.isRequired,
+    children: React.PropTypes.any,
+  };
+
+  return connect(
+    state => ({ sort: state.sort })
+  )(wrappedComponent);
+};
+
 export default function createSortableColumn({
   description,
   title,
   key,
 }) {
-  const connectToStore = connect(
-    state => ({ sort: state.sort }),
-    { setSort: setSortAction }
-  );
-
-  const baseClasses = {
-    [`column-${key}`]: true,
-  };
-
-  const connectHeaderCell = component => {
-    const wrappedComponent = ({ sort, setSort, children }) => {
-      // TODO: Tooltip / aria-label
-
-      const isSorted = sort.key === key;
-      const classes = Object.assign({
-        sortable: true,
-        sorted: isSorted,
-        ascending: isSorted && !sort.descending,
-        descending: isSorted && sort.descending,
-      }, baseClasses);
-
-      return React.createElement(component, {
-        toggleSort: createSortToggler(key, sort, setSort),
-        className: classNames(classes),
-        tooltip: createSortHeaderTooltip(description || title, classes),
-        title,
-      }, children);
-    };
-
-    wrappedComponent.propTypes = {
-      sort: React.PropTypes.object.isRequired,
-      setSort: React.PropTypes.func.isRequired,
-      children: React.PropTypes.any,
-    };
-
-    return connectToStore(wrappedComponent);
-  };
-
-  const connectDataCell = component => {
-    const wrappedComponent = ({ sort, result, children }) => {
-      const classes = Object.assign({
-        cell: true,
-        sorted: sort.key === key,
-      }, baseClasses);
-
-      return React.createElement(component, {
-        className: classNames(classes),
-        value: result[key],
-        result,
-      }, children);
-    };
-
-    wrappedComponent.propTypes = {
-      sort: React.PropTypes.object.isRequired,
-      result: React.PropTypes.object.isRequired,
-      children: React.PropTypes.any,
-    };
-
-    return connectToStore(wrappedComponent);
-  };
-
+  const connectHeaderCell = createHeaderCellConnector(description, title, key);
+  const connectDataCell = createDataCellConnector(key);
   const HeaderCell = connectHeaderCell(GenericHeaderCell);
   const DataCell = connectDataCell(GenericDataCell);
 
