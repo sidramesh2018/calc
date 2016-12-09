732718de6e7d9515927d46d72e39463ae8ca9bad
Atul Varma <varmaa@gmail.com>
2016-12-09 07:52:49 -0500

    Give proposed price field a proper label. Fixes #547.

--- frontend/source/js/data-explorer/components/proposed-price.jsx
+++ frontend/source/js/data-explorer/components/proposed-price.jsx
@@ -53,18 +53,18 @@ class ProposedPrice extends React.Component {
   }
 
   render() {
-    // TODO: Map the label to the input via htmlFor.
-
     // Note that Chrome has issues with number input, so we're just
     // going to use a standard text field here. This is unfortunate for
     // mobile users, since they won't get a numeric keypad.
     //
     // See more details at: https://github.com/facebook/react/issues/1549
 
+    const id = `${this.props.idPrefix}proposed-price`;
+
     return (
       <div className="proposed-price">
-        <label className="sr-only">Proposed price</label>
-        <input type="text" name="proposed-price"
+        <label htmlFor={id} className="sr-only">Proposed price</label>
+        <input id={id} type="text" name="proposed-price"
                placeholder="Proposed price" value={this.state.typed}
                onChange={this.handleChange} />
         <button className="button-primary go">Go</button>
@@ -76,6 +76,11 @@ class ProposedPrice extends React.Component {
 ProposedPrice.propTypes = {
   proposedPrice: React.PropTypes.number.isRequired,
   dispatch: React.PropTypes.func.isRequired,
+  idPrefix: React.PropTypes.string,
+};
+
+ProposedPrice.defaultProps = {
+  idPrefix: '',
 };
 
 function mapStateToProps(state) {
