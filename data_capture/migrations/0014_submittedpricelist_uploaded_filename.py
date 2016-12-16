12d9d3f7c1c2fc45f6827b69dc0b7c4f96306088
Atul Varma <varmaa@gmail.com>
2016-12-16 06:44:28 -0500

    Make migration work atop ones added recently.

--- data_capture/migrations/0014_submittedpricelist_uploaded_filename.py
+++ data_capture/migrations/0014_submittedpricelist_uploaded_filename.py
@@ -7,14 +7,14 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data_capture', '0012_auto_20161207_1513'),
+        ('data_capture', '0013_auto_20161214_1517'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='submittedpricelist',
             name='uploaded_filename',
-            field=models.CharField(max_length=128, help_text="Name of the file that was uploaded, as it was called on the uploader's system. For display purposes only.", default='unnamed'),
+            field=models.CharField(default='unnamed', max_length=128, help_text="Name of the file that was uploaded, as it was called on the uploader's system. For display purposes only."),
             preserve_default=False,
         ),
     ]
