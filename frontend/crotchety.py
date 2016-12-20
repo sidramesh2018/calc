6cef587afd34d026164ffc17a71906f0090aa6f3
Atul Varma <varmaa@gmail.com>
2016-12-20 06:54:05 -0500

    Add frontend/crotchety.py to simulate production static file storage.

---
+++ frontend/crotchety.py
@@ -0,0 +1,30 @@
+# This middleware + static file storage backend makes development behave more
+# like production by disallowing references to nonexistent files from being
+# generated in templates.
+
+from django.contrib.staticfiles.storage import StaticFilesStorage
+from django.contrib.staticfiles import finders
+from whitenoise.middleware import WhiteNoiseMiddleware
+
+
+class CrotchetyWhiteNoiseMiddleware(WhiteNoiseMiddleware):
+    def get_name_without_hash(self, filename):
+        # The default implementation of this sometimes returns
+        # the names of nonexistent files (e.g. converting files with
+        # .min.js extensions to just .js) which then makes the static
+        # file storage backend throw an exception.
+        #
+        # Since this middleware should only be used in development,
+        # where hashes are never included as part of filenames, we'll
+        # just return the filename here.
+        return filename
+
+
+class CrotchetyStaticFilesStorage(StaticFilesStorage):
+    def url(self, name):
+        foundpath = finders.find(name)
+
+        if not foundpath:
+            raise Exception('Cannot find %s' % name)
+
+        return super().url(name)
