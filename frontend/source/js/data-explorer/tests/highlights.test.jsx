4bf17019febecc14e05149505c9885bdc2a92fb4
James Seppi <james.seppi@gsa.gov>
2016-12-21 15:57:51 -0600

    add snapshot test

--- frontend/source/js/data-explorer/tests/highlights.test.jsx
+++ frontend/source/js/data-explorer/tests/highlights.test.jsx
@@ -1,6 +1,7 @@
 /* global expect, describe, it */
 import React from 'react';
 import { render } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 import { Highlights } from '../components/highlights.jsx';
 
@@ -20,12 +21,17 @@ function setup() {
 }
 
 describe('<Highlights>', () => {
-  it('should render self', () => {
+  it('renders correctly', () => {
     const { wrapper } = setup();
 
     expect(wrapper.find('.sd-highlight').first().text()).toBe('$1');
     expect(wrapper.find('.avg-price-highlight').text()).toBe('$2');
     expect(wrapper.find('.sd-highlight').last().text()).toBe('$3');
     expect(wrapper.find('.proposed-price-highlight').text()).toBe('$5');
+  });
+
+  it('matches snapshot', () => {
+    const { wrapper } = setup();
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
