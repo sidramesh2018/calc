e6550e91d255c1a0ff130603775aa606c3853e75
Atul Varma <varmaa@gmail.com>
2016-12-29 14:52:09 -0500

    Ensure all config files use same python version.

---
+++ hourglass/tests/test_environment.py
@@ -0,0 +1,55 @@
+import os
+from unittest import TestCase
+import yaml
+
+
+MY_DIR = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = os.path.normpath(os.path.join(MY_DIR, '..', '..'))
+
+
+def path(*x):
+    return os.path.join(ROOT_DIR, *x)
+
+
+class Version:
+    '''
+    Just a helper that makes it easy to do things with version numbers.
+
+        >>> v = Version('1.2.3')
+        >>> str(v)
+        '1.2.3'
+        >>> float(v)
+        1.2
+    '''
+
+    def __init__(self, version):
+        self.parts = version.split('.')
+
+    def __str__(self):
+        return '.'.join(self.parts)
+
+    def __float__(self):
+        return float('.'.join(self.parts[:2]))
+
+
+class PythonVersionTests(TestCase):
+    '''
+    Ensure all our configuration files specify the same Python version.
+    '''
+
+    version = Version('3.4.5')
+
+    def test_runtime_txt(self):
+        with open(path('runtime.txt')) as f:
+            self.assertEqual(f.read().strip(),
+                             'python-{}'.format(self.version))
+
+    def test_dockerfile(self):
+        with open(path('Dockerfile')) as f:
+            self.assertIn('FROM python:{}'.format(self.version),
+                          f.read())
+
+    def test_travis_yml(self):
+        with open(path('.travis.yml')) as f:
+            data = yaml.load(f)
+            self.assertEqual(data['python'], [float(self.version)])
