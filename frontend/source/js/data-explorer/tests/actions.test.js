d868a03d1f02bdb7a8e7f438d36cc8e2ab13e879
Atul Varma <varmaa@gmail.com>
2016-12-30 10:10:32 -0500

    add actions.test.js.

---
+++ frontend/source/js/data-explorer/tests/actions.test.js
@@ -0,0 +1,18 @@
+/* global expect, describe, it, jest */
+import * as actions from '../actions';
+
+describe('actions', () => {
+  it('should create an action to exclude a row', () => {
+    const rowId = 12345;
+    expect(actions.excludeRow(rowId)).toEqual({
+      type: actions.EXCLUDE_ROW,
+      rowId,
+    });
+  });
+
+  it('should create an action to exclude none', () => {
+    expect(actions.excludeNone()).toEqual({
+      type: actions.EXCLUDE_NONE,
+    });
+  });
+});
