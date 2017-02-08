a579c94e9410d15e4caf9e78454a7b6e6ece0800
Atul Varma <varmaa@gmail.com>
2017-02-08 08:57:22 -0500

    Factor out an analyze_gleaned_data function.

--- data_capture/templatetags/analyze_contract.py
+++ data_capture/templatetags/analyze_contract.py
@@ -1,4 +1,5 @@
 import math
+import datetime
 from urllib.parse import urlencode
 from itertools import chain, combinations
 from functools import cmp_to_key
@@ -9,10 +10,11 @@ except ImportError:
     nltk = None
 
 from django import template
-from django.db import connection
+from django.db import connection, transaction
 from django.db.models import Avg, StdDev
 from django.template.loader import render_to_string
 
+from data_capture.models import SubmittedPriceList
 from contracts.models import Contract
 from contracts.models import EDUCATION_CHOICES as _EDUCATION_CHOICES
 
@@ -306,6 +308,45 @@ def describe(cursor, vocab, labor_category, min_years_experience,
         )
 
     return result
+
+
+def analyze_gleaned_data(gleaned_data):
+    valid_rows = []
+
+    if gleaned_data.valid_rows:
+        cursor = connection.cursor()
+        vocab = Vocabulary.from_db(cursor)
+        with transaction.atomic():
+            sid = transaction.savepoint()
+            price_list = SubmittedPriceList(
+                is_small_business=False,
+                submitter_id=0,
+                escalation_rate=0,
+                status_changed_at=datetime.datetime.now(),
+                status_changed_by_id=0,
+            )
+            price_list.save()
+            gleaned_data.add_to_price_list(price_list)
+            for row in price_list.rows.all():
+                analysis = describe(
+                    cursor,
+                    vocab,
+                    row.labor_category,
+                    row.min_years_experience,
+                    row.education_level,
+                    float(row.base_year_rate),
+                )
+                valid_rows.append({
+                    'analysis': analysis,
+                    'sin': row.sin,
+                    'labor_category': row.labor_category,
+                    'education_level': row.education_level,
+                    'min_years_experience': row.min_years_experience,
+                    'price': float(row.base_year_rate),
+                })
+            transaction.savepoint_rollback(sid)
+
+    return valid_rows
 
 
 @register.assignment_tag(takes_context=True)
