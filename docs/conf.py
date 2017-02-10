826203ea89eea61d0a5b35e1cfc4f48206e3255a
Atul Varma <varmaa@gmail.com>
2017-02-10 10:10:03 -0500

    fix flake8 errors introduced by sphinx-quickstart.

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,10 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -160,9 +162,6 @@ texinfo_documents = [
      author, 'CALC', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
