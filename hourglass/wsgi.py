50359a2afdc6bc5ebda0d6f8dd919faeaea09414
Atul Varma <varmaa@gmail.com>
2017-02-16 08:20:58 -0500

    Serve docs from /docs/ instead of /static/docs/.

--- hourglass/wsgi.py
+++ hourglass/wsgi.py
@@ -27,7 +27,38 @@ if nr_license and nr_app_name:
     nr_settings.app_name = nr_app_name
     newrelic.agent.initialize()
 
+
+def static_url_rewriter(app, src_prefix, dest_prefix):
+    '''
+    WSGI middleware to rewrite the `PATH_INFO` of the WSGI
+    environment based on a source and destination prefix.
+
+    The content being served from the site is assumed to be static
+    content, so `index.html` is appended to the path if needed.
+    '''
+
+    def wrapper(environ, start_response):
+        path = environ['PATH_INFO']
+        if path == src_prefix[:-1]:
+            start_response('302 Found', [
+                ('Location', src_prefix)
+            ])
+            return []
+        elif path.startswith(src_prefix):
+            path_info = dest_prefix + path[len(src_prefix):]
+            if path_info.endswith('/'):
+                path_info += 'index.html'
+            environ['PATH_INFO'] = path_info
+        return app(environ, start_response)
+
+    return wrapper
+
+
 try:
-    application = get_wsgi_application()
+    application = static_url_rewriter(
+        get_wsgi_application(),
+        '/docs/',
+        '/static/docs/'
+    )
 except Exception as e:
     print(e)
