e5ca2d024526e884e356619f188a732fae48192a
Atul Varma <varmaa@gmail.com>
2017-03-01 11:46:38 -0500

    Run update_search_field in update scripts.

--- update.sh
+++ update.sh
@@ -11,5 +11,9 @@ npm install
 echo "----- Migrating Database -----"
 python manage.py migrate --noinput
 
+echo "----- Updating search field -----"
+# The '-W ignore is to suppress https://github.com/18F/calc/issues/291.
+python -W ignore manage.py update_search_field contracts
+
 echo "----- Initializing Groups -----"
 python manage.py initgroups
