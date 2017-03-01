b3d11345e8375917e40bb35497fa2f029da43d58
James Seppi <james.seppi@gsa.gov>
2017-02-28 19:06:03 -0600

    use format utils for tz_timestamp

--- data_capture/templatetags/data_capture.py
+++ data_capture/templatetags/data_capture.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django import template
+from django.utils.formats import date_format, time_format
 from pytz import timezone
 
 register = template.Library()
@@ -8,14 +10,15 @@ register = template.Library()
 def tz_timestamp(value, tz_name="US/Eastern"):
     '''
     This template filter formats the given timezone-aware datetime into
-    a more readable format (ex: "Jan. 1, 2015 at 1:55 PM (EST)"),
-    localized to the timezone specified by tz_name.
+    a more readable format, localized to the timezone specified by tz_name.
+
+    >>> tz_timestamp(datetime(2016, 2, 11, 14, 30, 5))
+    'Feb. 11, 2016 at 9:30 a.m. (EST)'
     '''
     try:
         tz = timezone(tz_name)
         timestamp = value.astimezone(tz)
-        fmt = '%b. %-d, %Y at %-I:%M %p'
-        return f"{timestamp.strftime(fmt)} ({timestamp.tzname()})"
+        return f"{date_format(timestamp)} at {time_format(timestamp)} ({timestamp.tzname()})"
     except (AttributeError, ValueError):
         # just return the original value
         return value
--- data_capture/tests/test_templatetags.py
+++ data_capture/tests/test_templatetags.py
@@ -10,7 +10,7 @@ class ReadableTimestampTests(TestCase):
     def test_it_works(self):
         utc_datetime = datetime(2016, 2, 3, 14, 30, 5)
         val = tz_timestamp(utc_datetime)
-        self.assertEqual(val, 'Feb. 3, 2016 at 9:30 AM (EST)')
+        self.assertEqual(val, 'Feb. 3, 2016 at 9:30 a.m. (EST)')
 
     def test_it_returns_orig_value_when_not_datetime(self):
         val = tz_timestamp('abc')
@@ -19,7 +19,7 @@ class ReadableTimestampTests(TestCase):
     def test_timezone_name_can_be_specified(self):
         utc_datetime = datetime(2016, 2, 11, 14, 30, 5)
         val = tz_timestamp(utc_datetime, "US/Central")
-        self.assertEqual(val, 'Feb. 11, 2016 at 8:30 AM (CST)')
+        self.assertEqual(val, 'Feb. 11, 2016 at 8:30 a.m. (CST)')
 
 
 @override_settings(SECURE_SSL_REDIRECT=False)
