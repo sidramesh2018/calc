ae21dfcf4860aa9765a3198ae762b6c248d60504
James Seppi <james.seppi@gsa.gov>
2017-03-02 16:21:49 -0600

    rm obviated autocomplete.js and related

--- frontend/source/js/tests/data-explorer_tests.js
+++ frontend/source/js/tests/data-explorer_tests.js
@@ -1,46 +1,10 @@
 /* global QUnit document $ */
-import { appendHighlightedTerm } from '../data-explorer/autocomplete';
 import {
   parseQuery,
   joinQuery,
 } from '../data-explorer/util';
 
 QUnit.module('data-explorer');
-
-QUnit.test('appendHighlightedTerm() prevents stored XSS', (assert) => {
-  assert.equal(
-    appendHighlightedTerm(
-      $('<span></span>'),
-      'Project Manager of <script>alert("DOOM")</script>',
-      'proj manager',
-    ).html(),
-    '<b>Proj</b>ect <b>Manager</b> of &lt;script&gt;alert("DOOM")&lt;/script&gt;',
-  );
-});
-
-QUnit.test('appendHighlightedTerm() works', (assert) => {
-  assert.equal(
-    appendHighlightedTerm(
-      $('<span></span>'),
-      'superduperman',
-      'super').html(),
-    '<b>super</b>duperman');
-
-  assert.equal(
-    appendHighlightedTerm(
-      $('<span></span>'),
-      'superduperman',
-      'uper').html(),
-    's<b>uper</b>d<b>uper</b>man');
-
-  // test to make sure it doesn't blow up on special-character-only search term
-  assert.equal(
-    appendHighlightedTerm(
-      $('<span></span>'),
-      'superduperman',
-      '{{{').html(),
-    'superduperman');
-});
 
 QUnit.test('parseQuery() works', (assert) => {
   assert.deepEqual(
