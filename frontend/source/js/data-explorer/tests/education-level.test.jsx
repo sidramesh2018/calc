900086fe8408d94e5581a97762e5b7d5ca20cb1b
Heather Battaglia <heather.battaglia@gsa.gov>
2017-03-03 07:04:28 -0500

    add test for education-level toggle menu

--- frontend/source/js/data-explorer/tests/education-level.test.jsx
+++ frontend/source/js/data-explorer/tests/education-level.test.jsx
@@ -54,4 +54,16 @@ describe('<EducationLevel>', () => {
     expect(props.toggleEducationLevel.mock.calls.length).toBe(1);
     expect(props.toggleEducationLevel.mock.calls[0][0]).toBe('HS');
   });
+
+  it('fires handleToggleMenu when dropdown is clicked', () => {
+    const { wrapper } = setup();
+    const e = {
+      preventDefault: jest.fn()
+    }
+    wrapper.state().expanded = false;
+    expect(wrapper.state().expanded).toBeFalsy();
+    wrapper.find('a').simulate('click', e);
+    expect(wrapper.state().expanded).toBeTruthy();
+  });
+
 });
