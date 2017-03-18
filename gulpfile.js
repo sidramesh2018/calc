ab90861d834ba2432fa071af2ce2c8589bc8ff3b
Atul Varma <varmaa@gmail.com>
2017-03-17 20:57:09 -0400

    Create frontend/gulp/ dir, add tests for webpack.

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,10 @@ const rename = require('gulp-rename');
 const eslint = require('gulp-eslint');
 const gutil = require('gulp-util');
 const del = require('del');
-const webpackStream = require('webpack-stream');
-const webpack = require('webpack');
-const named = require('vinyl-named');
 const plumber = require('gulp-plumber');
+const named = require('vinyl-named');
+
+const webpackUtil = require('./frontend/gulp/webpack-util');
 
 const BUILT_FRONTEND_DIR = 'frontend/static/frontend/built';
 
@@ -181,62 +181,14 @@ gulp.task('set-watching', () => {
   isWatching = true;
 });
 
-gulp.task('js:webpack', () => {
-  const sources = Object.keys(bundles).map((name) => {
-    const options = bundles[name];
-    const dirName = options.dirName || name;
-    return path.join(dirs.src.scripts, dirName, 'index.js');
-  });
-
-  const plugins = [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': isProd ? '"production"' : '""',
-    }),
-  ];
-
-  if (isProd) {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-    }));
-  }
-
-  return gulp.src(sources)
-    .pipe(plumber())
-    .pipe(named((file) => {
-      // All of our bundle entry files are of the form  "/some/path/index.js".
-      // We'll use the last folder name to generate the file name of the
-      // bundles (ie, "path.js") so that they're not all called "index.js".
-      const parts = file.path.split(path.sep);
-      const lastFolderName = parts[parts.length - 2];
-      return lastFolderName;
-    }))
-    .pipe(webpackStream({
-      watch: isWatching,
-      resolve: {
-        extensions: ['.js', '.jsx'],
-      },
-      context: __dirname,
-      node: {
-        __filename: true,
-      },
-      devtool: isProd ? 'source-map' : 'eval-source-map',
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-              presets: ['es2015', 'react'],
-              cacheDirectory: true,
-            },
-          },
-        ],
-      },
-      plugins,
-    }, webpack))
-    .pipe(gulp.dest(`${BUILT_FRONTEND_DIR}/js/`));
-});
+gulp.task('js:webpack', () => gulp.src(webpackUtil.scriptSources({
+  bundles,
+  rootDir: dirs.src.scripts,
+}))
+  .pipe(plumber())
+  .pipe(named(webpackUtil.getLastFolderName))
+  .pipe(webpackUtil.webpackify({ isWatching, isProd }))
+  .pipe(gulp.dest(`${BUILT_FRONTEND_DIR}/js/`)));
 
 gulp.task('lint', () => gulp.src(path.join(dirs.src.scripts, paths.js))
     .pipe(eslint())
