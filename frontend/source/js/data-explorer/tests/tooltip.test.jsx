eaa044887dff6d8a55ac422cbb9ed023568c3f2e
Heather Battaglia <heather.battaglia@gsa.gov>
2017-03-20 12:37:47 -0600

    shouldn't have had that wip tooltip bit in there

--- frontend/source/js/data-explorer/tests/tooltip.test.jsx
+++ frontend/source/js/data-explorer/tests/tooltip.test.jsx
@@ -33,13 +33,4 @@ describe('<Tooltip>', () => {
     expect(tooltipsterMock.mock.calls.length).toBeGreaterThan(0);
   });
 
-  it('calls the show and hide functions in respect to the right props', () => {
-    const { wrapper } = setup();
-    // mock the tooltipster show/hide functions
-    const show = jest.fn();
-    const hide = jest.fn();
-    const isHidden = false; // same as defaultProps.show
-    wrapper.syncShowProps();
-    expect(show.mock.calls.length).toBe(1);
-  });
 });
