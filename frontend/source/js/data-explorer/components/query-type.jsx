5b68c6f55b956d9e14ae851b7cec3b6e2aeb92b7
Atul Varma <varmaa@gmail.com>
2017-04-14 11:26:38 -0400

    Run aXe-core on data explorer, fix a11y violation.

--- frontend/source/js/data-explorer/components/query-type.jsx
+++ frontend/source/js/data-explorer/components/query-type.jsx
@@ -41,7 +41,7 @@ export function QueryType({ queryType, setQueryType, idPrefix }) {
   };
 
   return (
-    <div>
+    <div role="group" aria-label="Labor category query type">
       {input(QUERY_TYPE_MATCH_ALL)}
       {input(QUERY_TYPE_MATCH_PHRASE)}
       {input(QUERY_TYPE_MATCH_EXACT)}
