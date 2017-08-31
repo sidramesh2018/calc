316201a07007bb683a038dc01858745fef11f746
James Seppi <james.seppi@gsa.gov>
2017-08-31 09:47:37 -0500

    fix flake8 errors

--- api/tests/test_rates_csv.py
+++ api/tests/test_rates_csv.py
@@ -5,14 +5,15 @@ from .test_rates_api import GetRatesTests
 
 RATES_CSV_PATH = '/api/rates/csv'
 
+
 # TODO: These tests need to be more fully fleshed out
 class GetRatesCSVTests(TestCase):
-  def setUp(self):
-    GetRatesTests.make_test_set()
-    self.path = RATES_CSV_PATH
+    def setUp(self):
+        GetRatesTests.make_test_set()
+        self.path = RATES_CSV_PATH
 
-  def test_prefixes_excel_formula_chars(self):
-    excel_formula_prefixes = ['@', '-', '=', '|', '%']
-    for char in excel_formula_prefixes:
-      resp = self.client.get(f'{self.path}/?q={char}query')
-      self.assertContains(resp, f"'{char}query")
+    def test_prefixes_excel_formula_chars(self):
+        excel_formula_prefixes = ['@', '-', '=', '|', '%']
+        for char in excel_formula_prefixes:
+            resp = self.client.get(f'{self.path}/?q={char}query')
+            self.assertContains(resp, f"'{char}query")
