38797c230de3fe959ac9633b2f343bd2354a0cf6
Atul Varma <varmaa@gmail.com>
2018-04-17 14:41:33 -0400

    Add fullpage example tests.

--- styleguide/fullpage_example.py
+++ styleguide/fullpage_example.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from django.utils.safestring import SafeString
 from django.shortcuts import render
 from django.urls import reverse
+from django.http import Http404
 
 
 MY_DIR = Path(__file__).parent.resolve()
@@ -11,16 +12,25 @@ EXAMPLES_DIR = MY_DIR / 'templates' / 'styleguide_fullpage_examples'
 REGEX = r'([0-9A-Za-z_\-]+)'
 
 
+def to_path(name):
+    return EXAMPLES_DIR / f'{name}.html'
+
+
 def get_html_source(name):
-    filename = EXAMPLES_DIR / f'{name}.html'
-    return SafeString(filename.read_text())
+    return SafeString(to_path(name).read_text())
 
 
 def get_url(name):
     return reverse('styleguide:fullpage_example', args=(name,))
 
 
+def exists(name):
+    return to_path(name).exists()
+
+
 def view(request, name):
+    if not exists(name):
+        raise Http404("fullpage example does not exist")
     html = get_html_source(name)
     return render(request, 'styleguide_fullpage_example.html', {
         'html': html,
