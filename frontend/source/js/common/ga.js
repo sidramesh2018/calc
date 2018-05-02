4469ae4d89f279d5a0ebd64b73e6aa1fdf5033a6
Atul Varma <varmaa@gmail.com>
2018-05-02 19:30:27 -0400

    Emit virtual pageviews to DAP and our GA.

--- frontend/source/js/common/ga.js
+++ frontend/source/js/common/ga.js
@@ -1,4 +1,20 @@
-/* global window */
+// @ts-check
+/* eslint-env browser */
+
+/**
+ * This is DAP's weird exported Google Analytics function, documented
+ * at:
+ *
+ *     https://github.com/digital-analytics-program/gov-wide-code
+ *
+ */
+function gas(...args) {
+  const origGas = window['gas'];         // eslint-disable-line dot-notation
+  if (typeof origGas === 'function') {
+    return origGas.apply(this, args);
+  }
+  return undefined;
+}
 
 /**
  * This is just a wrapper around Google Analytics' ga() function, which
@@ -8,11 +24,24 @@
  * dependency.
  */
 
-function ga(...args) {
-  if (typeof window.ga === 'function') {
-    return window.ga.apply(this, args);
+export default function ga(...args) {
+  const origGa = window['ga'];         // eslint-disable-line dot-notation
+  if (typeof origGa === 'function') {
+    return origGa.apply(this, args);
   }
   return undefined;
 }
 
-module.exports = ga;
+/**
+ * Track a "virtual pageview" using Google Analytics. Typically
+ * used in single-page apps and such.
+ *
+ * @param {string} [url] The URL of the page to track, e.g. '/boop'.
+ *   Defaults to the current page's URL, including the current
+ *   search query.
+ */
+export function trackVirtualPageview(url) {
+  ga('set', 'page', url || location.pathname + location.search);
+  ga('send', 'pageview');
+  gas('send', 'pageview', url);
+}
