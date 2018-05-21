190d148e0b4f996c6a35ab9d5406a2b49abcc446
Atul Varma <varmaa@gmail.com>
2018-05-21 06:21:54 -0400

    Add test for detail page.

--- contracts/tests/test_reports.py
+++ contracts/tests/test_reports.py
@@ -7,6 +7,12 @@ from contracts.mommy_recipes import get_contract_recipe
 class Tests(TestCase):
     def setUp(self):
         get_contract_recipe().make(_quantity=10)
+        get_contract_recipe().make(
+            labor_category='senior outlier',
+            min_years_experience=1,
+            education_level='HS',
+            current_price=999999
+        )
 
     def test_data_quality_report_command_does_not_explode(self):
         call_command('data_quality_report')
@@ -14,3 +20,7 @@ class Tests(TestCase):
     def test_data_quality_report_view_does_not_explode(self):
         res = self.client.get('/data-quality-report/')
         self.assertEqual(res.status_code, 200)
+
+    def test_data_quality_report_detail_view_does_not_explode(self):
+        res = self.client.get('/data-quality-report/outliers/')
+        self.assertContains(res, 'senior outlier')
