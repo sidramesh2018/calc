074c54d7a6d4bbc56c08e541b28ac99a94ef903d
Heather Battaglia <heather.battaglia@gsa.gov>
2018-08-13 10:57:02 -0700

    split 'legacy' from the schedule name; style radios

--- frontend/source/js/data-explorer/components/schedule.jsx
+++ frontend/source/js/data-explorer/components/schedule.jsx
@@ -8,9 +8,23 @@ import { scheduleLabels } from '../schedule-metadata';
 
 export function Schedule({ idPrefix, selectedSchedule, setSchedule }) {
   const handleChange = (e) => { setSchedule(e.target.value); };
-  const defaultMsg = `In all (${scheduleLabels.keys}) contract vehicles`;
+  const defaultMsg = `all (${Object.keys(scheduleLabels).length}) contract vehicles`;
+  // In most instances, we display legacy schedules as "Legacy Schedule," i.e., "Legacy MOBIS."
+  // Here, however, we want to display the "Legacy" modifier in parenthesis after the name.
+  // Since the legacy modifier only is found in schedule.full_name, we have to regex.
+  const legacyPrefix = "Legacy "
   const makeInput = (value, label) => {
     const id = value.replace(/ /g, '-').toLowerCase();
+    const makeLabel = (label) => {
+      let scheduleLabel = label;
+      let labelSuffix;
+      if (label.includes(legacyPrefix)) {
+        scheduleLabel = label.replace(legacyPrefix, '');
+        labelSuffix = "(Legacy)";
+      }
+      return {scheduleLabel, labelSuffix};
+    };
+    const { scheduleLabel, labelSuffix } = makeLabel(label);
     return (
       <li>
         <input
@@ -22,7 +36,10 @@ export function Schedule({ idPrefix, selectedSchedule, setSchedule }) {
           checked={selectedSchedule === value}
         />
         <label htmlFor={id}>
-          {label}
+          {scheduleLabel}
+          <span className="label__suffix">
+            {labelSuffix}
+          </span>
         </label>
       </li>
     );
