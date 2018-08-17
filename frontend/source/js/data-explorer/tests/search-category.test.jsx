7c88e4b94957a81b3996b4602a5b4b13da57b488
Heather Battaglia <heather.battaglia@gsa.gov>
2018-08-17 15:02:41 -0600

    add very basic test for search dropdown

---
+++ frontend/source/js/data-explorer/tests/search-category.test.jsx
@@ -0,0 +1,19 @@
+import { SearchCategory } from '../components/search-category';
+import { Schedule } from '../components/schedule';
+import makeSetup from './testSetup';
+
+const defaultProps = {
+  selectedSchedule: '',
+};
+
+const setup = makeSetup(SearchCategory, defaultProps, {wrapperOnly: true});
+
+describe('<SearchCategory>', () => {
+  it('renders correctly', () => {
+    const { wrapper } = setup();
+    const trigger = wrapper.find('.html-dropdown__trigger');
+    expect(trigger.exists()).toBeTruthy();
+    const dropdown = wrapper.find('.html-dropdown__choices');
+    expect(dropdown.exists()).toBeTruthy();
+  });
+});
