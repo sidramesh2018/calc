72258f81d47b57f706e448e1969a38cf6d641f05
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2018-09-27 13:42:42 -0700

    Add download of NLTK resources to cloud.gov deploy/setup script.

--- cf.sh
+++ cf.sh
@@ -11,6 +11,17 @@ if [ $CF_INSTANCE_INDEX = "0" ]; then
 
     echo "----- Initializing Groups -----"
     python manage.py initgroups
+
+    echo "----- Downloading NLTK Data -----"
+
+    if [ -d "${NLTK_DATA_DIR}" ]; then
+      NLTK_OPTS="-d ${NLTK_DATA_DIR}"
+    else
+      NLTK_OPTS=""
+    fi
+
+    python -m nltk.downloader averaged_perceptron_tagger ${NLTK_OPTS}
+
 fi
 echo "------ Starting APP ------"
 gunicorn calc.wsgi:application
